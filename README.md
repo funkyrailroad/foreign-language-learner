bec97b338141f5fbae3690b4bd97b852c83a7468
Jake Atwell <jatwell@alexinc.com>
2024-04-05 22:08:55 -0400

    Use env vars for pg connection settings

--- README.md
+++ README.md
@@ -1,11 +1,5 @@
 # Instructions
 
-Enter correct database credentials and rename them
-```
-./mysite/pgpass -> ./mysite/.pgpass
-./mysite/pg_service.conf -> ./mysite/.pg_service.conf
-```
-
 Start it up:
 ```
 docker compose up -d
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,10 +76,11 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "OPTIONS": {
-            "service": "my_service",
-            "passfile": ".pgpass",
-        },
+        "HOST": os.environ["DB_HOST"],
+        "NAME": os.environ["DB_NAME"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "PORT": os.environ["DB_PORT"],
+        "USER": os.environ["DB_USER"],
     }
 }
 
