52cd7550affb584e0d671c24d31141dcd83fe29f
Jake Atwell <jatwell@alexinc.com>
2024-04-20 01:15:26 -0400

    Create translation functions and tests

--- mysite/fll/tests.py
+++ mysite/fll/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
-# Create your tests here.
+import fll.util as u
+
+
+class TranslationTests(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.text = "I want to learn foreign languages."
+
+    def test_translate_to_german(self):
+        gt_translation = "Ich möchte Fremdsprachen lernen."
+
+        translation = u.translate_to_german(self.text)
+        self.assertEqual(translation, gt_translation)
+
+    def test_translate_to_italian(self):
+        gt_translation = "Voglio imparare le lingue straniere."
+
+        translation = u.translate_to_italian(self.text)
+        self.assertEqual(translation, gt_translation)
+
+    def test_translate_to_spanish(self):
+        gt_translation = "Quiero aprender idiomas extranjeros."
+
+        translation = u.translate_to_spanish(self.text)
+        self.assertEqual(translation, gt_translation)
+
+    def test_translate_to_swahili(self):
+        gt_translation = "Nataka kujifunza lugha za kigeni."
+
+        translation = u.translate_to_swahili(self.text)
+        self.assertEqual(translation, gt_translation)
---
+++ mysite/fll/util.py
@@ -0,0 +1,22 @@
+import os
+import whisper
+import translators as ts
+
+
+def transcribe_audio_with_whisper(fp):
+    result = model.transcribe(fp)
+    return result["text"].strip()
+
+# model = whisper.load_model("base.en")
+
+def translate_to_german(text):
+    return ts.translate_text(text, to_language="de")
+
+def translate_to_italian(text):
+    return ts.translate_text(text, to_language="it")
+
+def translate_to_spanish(text):
+    return ts.translate_text(text, to_language="es")
+
+def translate_to_swahili(text):
+    return ts.translate_text(text, to_language="sw")
