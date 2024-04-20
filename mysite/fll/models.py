c6a8d4d4f7386b997a9c8fb3bc64212c21904c30
Jake Atwell <jatwell@alexinc.com>
2024-04-20 01:16:44 -0400

    Create AudioNote model

--- mysite/fll/models.py
+++ mysite/fll/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class AudioNote(models.Model):
+    audio_hash = models.CharField(max_length=100, blank=True)
+    english = models.TextField(blank=True)
+    german = models.TextField(blank=True)
+    italian = models.TextField(blank=True)
+    spanish = models.TextField(blank=True)
+    swahili = models.TextField(blank=True)
